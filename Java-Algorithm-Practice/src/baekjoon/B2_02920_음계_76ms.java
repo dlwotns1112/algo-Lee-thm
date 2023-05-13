a88a81ef749d5f95916319d06a677113f7b26de2
이재순 <dlwotns1112@gmail.com>
2023-05-13 13:37:18 +0900

    230513_BOJ_B2_02920_음계

---
+++ Java-Algorithm-Practice/src/baekjoon/B2_02920_음계_76ms.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2_02920_음계_76ms {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        if (line.equals("1 2 3 4 5 6 7 8"))
+            System.out.println("ascending");
+        else if (line.equals("8 7 6 5 4 3 2 1"))
+            System.out.println("descending");
+        else
+            System.out.println("mixed");
+    }
+}
